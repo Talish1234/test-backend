2f0b9dd5774dc7a7e74267b7eb7fb157e7165f46
Talish1234 <talishtarik1234@gmail.com>
2024-08-20 02:09:59 +0530

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser')
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const BASE = process.env.BASE;
+app.use(cors({
+    origin:BASE,
+    credentials:true
+}))
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/cookie", (req,res) => {
+  
+    res.cookie("token", "123341noefn243n", {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }).status(200).json({
+        success: true,
+        error: false
+    });
+});
+
+app.get('/getcookie',(req,res)=> {
+    const val = req.cookies.token;
+    res.json(val);
+},)
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on port`);
+  });
