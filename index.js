459f35fdce76a66fb8189357bf5fd0aef659ebff
Talish1234 <92112059+Talish1234@users.noreply.github.com>
2024-08-20 17:13:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 const BASE = process.env.BASE;
 app.use(cors({
     origin:BASE,
-    credentials:true
+    credentials:true,
+    exposedHeaders: ["token"] 
 }))
 
 
@@ -17,12 +18,12 @@ app.use(cookieParser());
 
 app.get("/cookie", (req,res) => {
   
-    res.cookie("token1", "123341noefn243n", {
+    res.cookie("token", "123341noefn243n", {
          secure: true,
          httpOnly:true,
          sameSite: 'None',
         partitioned: true,
-         maxAge: 1000 * 60 * 60 * 24 * 7
+        expires: new Date(Date.now() + 900000),
     }).status(200).json({
         success: true,
         error: false
@@ -31,9 +32,8 @@ app.get("/cookie", (req,res) => {
 
 app.get('/getcookie',(req,res)=> {
     const val = req.cookies.token;
-    const val2 = req.cookies.token1;
     
-    res.json({val,val2});
+    res.json({val});
 },)
 
 app.get('/*',(req,res)=> {
